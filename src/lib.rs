67c32d36fdb9ed91e95892cff1b7b7cee38c3872
onsdagens <pawdzi-7@student.ltu.se>
2024-03-13 16:36:58 +0100

    mintthresh

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,5 @@ pub mod i2_vec;
 pub mod interrupt0;
 pub mod interrupt1;
 pub mod interrupt2;
+pub mod mintthresh;
 pub mod mstatus;
-//pub use mstatus;
---
+++ src/mintthresh.rs
@@ -0,0 +1,8 @@
+pub struct Bits;
+
+impl Bits {
+    read_csr_as_usize!(0x347);
+    write_csr_as!(0x347);
+    set!(0x347);
+    clear!(0x347);
+}
