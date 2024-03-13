98cc28f191a7d64d2056ab44634a0ad1f1784935
onsdagens <pawdzi-7@student.ltu.se>
2024-03-13 13:44:08 +0100

    cleanup

--- hippomenes-derive/target/debug/build/proc-macro2-c6ebde767c1671df/out/proc_macro2.d
+++
@@ -1,7 +0,0 @@
-/home/pawel/hippomenes/hippomenes-core/hippomenes-derive/target/debug/build/proc-macro2-c6ebde767c1671df/out/libproc_macro2.rmeta: build/probe.rs
-
-/home/pawel/hippomenes/hippomenes-core/hippomenes-derive/target/debug/build/proc-macro2-c6ebde767c1671df/out/proc_macro2.d: build/probe.rs
-
-build/probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
--- hippomenes-derive/target/debug/libhippomenes_derive.d
+++
@@ -1,1 +0,0 @@
-/home/pawel/hippomenes/hippomenes-core/hippomenes-derive/target/debug/libhippomenes_derive.so: /home/pawel/hippomenes/hippomenes-core/hippomenes-derive/src/lib.rs
